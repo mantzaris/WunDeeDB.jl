b1aa94457e349c3f7a0390be099b3e354394fb61
mantzaris <avmantzaris@gmail.com>
2025-03-21 15:19:58 -0400

    now the lmdiskann code is adapted for sqlite backend rather than memory mapped but not tested yet

--- src/lmdiskann/LMDiskANN.jl
+++ src/lmdiskann/LMDiskANN.jl
@@ -1,9 +1,11 @@
 module LMDiskANN
 
 using Random
+using SQLite, DataFrames, DBInterface, JSON
+
 import ..WunDeeDB
 
-export build_index, search, insert!, delete!
+export search, insert!, delete!
 
 
 const DEFAULT_MAX_DEGREE = 64  # max number of neighbors
@@ -11,39 +13,41 @@ const SEARCH_LIST_SIZE   = 64  # search BFS/greedy queue size
 const EF_SEARCH          = 300 # search expansion factor
 const EF_CONSTRUCTION    = 400 # construction expansion factor
 
+const LM_DISKANN_CONFIG_TABLE_NAME = "LMDiskANNConfig"
+const LM_DISKANN_INDEX_TABLE_NAME = "LMDiskANNIndex"
+
 const INSERT_CONFIG_STMT = """
-    INSERT OR REPLACE INTO $(WunDeeDB.LM_DISKANN_CONFIG_TABLE_NAME) (entrypoint)
+    INSERT OR REPLACE INTO $(LM_DISKANN_CONFIG_TABLE_NAME) (entrypoint)
     VALUES (?);
 """
 
 const UPDATE_LM_DISKANN_CONFIG_STMT = """
-    UPDATE $(WunDeeDB.LM_DISKANN_CONFIG_TABLE_NAME)
+    UPDATE $(LM_DISKANN_CONFIG_TABLE_NAME)
     SET entrypoint = ?;
 """
 
 const GET_CONFIG_STMT = """
     SELECT *
-    FROM $(WunDeeDB.LM_DISKANN_CONFIG_TABLE_NAME)
+    FROM $(LM_DISKANN_CONFIG_TABLE_NAME)
     LIMIT 1;
 """
 
 const INSERT_LM_DISKANN_NEIGHBORS_STMT = """
-    INSERT OR REPLACE INTO $(WunDeeDB.LM_DISKANN_INDEX_TABLE_NAME) (id_text, neighbors)
+    INSERT OR REPLACE INTO $(LM_DISKANN_INDEX_TABLE_NAME) (id_text, neighbors)
     VALUES (?, ?);
 """
 
 const GET_NEIGHBORS_STMT = """
     SELECT neighbors
-    FROM $(WunDeeDB.LM_DISKANN_INDEX_TABLE_NAME)
+    FROM $(LM_DISKANN_INDEX_TABLE_NAME)
     WHERE id_text = ?;
 """
 
 const GET_LM_DISKANN_CONFIG_STMT = """
     SELECT *
-    FROM $(WunDeeDB.LM_DISKANN_CONFIG_TABLE_NAME)
+    FROM $(LM_DISKANN_CONFIG_TABLE_NAME)
     LIMIT 1;
 """
-
 
 
 
@@ -52,38 +56,55 @@ const GET_LM_DISKANN_CONFIG_STMT = """
 
 
 function _get_neighbors(db::SQLite.DB, node_id::String)
-    neighbors = []
-    for i in 1:index.maxdegree #TODO: use max degree constant
-        nbr_id = index.adjs[i, node_id+1] #TODO: get neighbors from db now
-        if nbr_id >= 0
-            push!(neighbors, nbr_id)
-        end
+    df = SQLite.execute(db, GET_NEIGHBORS_STMT, (node_id,)) |> DataFrame
+
+    if isempty(df)
+        return String[]
     end
+
+    neighbors_str = df[1, :neighbors]
+    
+    neighbors = JSON.parse(neighbors_str)
     return neighbors
 end
 
-#TODO: updates the DB
+
 function _set_neighbors(db::SQLite.DB, node_id::String, neighbor_ids::Vector{String})
-    maxd = index.maxdegree #TODO: get from db
+    maxd = DEFAULT_MAX_DEGREE
+
     if length(neighbor_ids) > maxd
         neighbor_ids = neighbor_ids[1:maxd] #update DB TODO: 
     end
 
+    neighbors_str = JSON.json(neighbor_ids)
+
+    DBInterface.execute(db, INSERT_LM_DISKANN_NEIGHBORS_STMT, (node_id, neighbors_str))
 end
 
 
 function _search_graph(db::SQLite.DB, query_vec::AbstractVector, ef::Int)
-    if index.entrypoint < 0 || index.num_points == 0
-        return Int[]
-    end
-    
-    visited    = Set()
-    candidates = []
-    results    = []
+    df = DBInterface.execute(db, "SELECT entrypoint FROM $(LM_DISKANN_CONFIG_TABLE_NAME) LIMIT 1") |> DataFrame
 
-    entry_id   = index.entrypoint #TODO: get entry point
-    entry_vec  = index.vecs[:, entry_id+1] #get entry vec TODO:
+    if isempty(df)
+        return String[]
+    end
+
+    entry_id = df[1, :entrypoint]
+    if isempty(entry_id)
+        return String[]
+    end
+
+    entry_dict = WunDeeDB.get_embeddings(db, [entry_id])
+    if !haskey(entry_dict, entry_id)
+        return String[]
+    end
+
+    entry_vec = entry_dict[entry_id]
     entry_dist = WunDeeDB.compute_distance(query_vec, entry_vec, "euclidean")
+   
+    visited    = Set{String}()
+    candidates = Vector{Tuple{Float64,String}}()
+    results    = Vector{Tuple{Float64,String}}()
 
     push!(visited, entry_id)
     push!(candidates, (entry_dist, entry_id))
@@ -97,14 +118,20 @@ function _search_graph(db::SQLite.DB, query_vec::AbstractVector, ef::Int)
             break
         end
         
-        neighbors = _get_neighbors(index, current_id) #TODO: get from db
+        neighbors = _get_neighbors(db, current_id)
+
         for nbr_id in neighbors
-            if nbr_id < 0 || (nbr_id in visited)
+            if nbr_id == "" || (nbr_id in visited)
                 continue
             end
             push!(visited, nbr_id)
 
-            nbr_vec = index.vecs[:, nbr_id+1] #TODO: get neighbor embedding
+            nbr_dict = WunDeeDB.get_embeddings(db, [nbr_id])
+            if !haskey(nbr_dict, nbr_id)
+                continue
+            end            
+
+            nbr_vec = nbr_dict[nbr_id]
             nbr_dist= WunDeeDB.compute_distance(query_vec, nbr_vec, "euclidean")
 
             sort!(results, by=x->x[1])
@@ -118,55 +145,71 @@ function _search_graph(db::SQLite.DB, query_vec::AbstractVector, ef::Int)
             end
         end
     end
+
     sort!(results, by=x->x[1])
     return [pair[2] for pair in results]
 end
 
 
-function search(db::SQLite.DB, query_vec::AbstractVector; topk::Int=10) #TODO: return only keys not internal / key
-
-    if index.num_points == 0 #TODO: use meta data from db
+function search(db::SQLite.DB, query_vec::AbstractVector; topk::Int=10)
+    entry_count = WunDeeDB.count_entries(db)
+    if entry_count == 0
         return []
     end
-    
-    ef_candidates = _search_graph(index, query_vec, max(topk, EF_SEARCH))
 
+    ef_candidates = _search_graph(db, query_vec, max(topk, EF_SEARCH))
     dist_id_pairs = []
 
     for cid in ef_candidates
-        v = index.vecs[:, cid+1]
-        d = WunDeeDB.compute_distance(query_vec, v, "euclidean")
+        cand_dict = WunDeeDB.get_embeddings(db, [cid])
+
+        if !haskey(cand_dict, cid)
+            continue
+        end
+        
+        cand_vec = cand_dict[cid]
+
+        d = WunDeeDB.compute_distance(query_vec, cand_vec, "euclidean")
         push!(dist_id_pairs, (d, cid))
     end
+
     sort!(dist_id_pairs, by=x->x[1])
 
     k = min(topk, length(dist_id_pairs))
-    results = []
-
-    for i in 1:k
-        cid = dist_id_pairs[i][2]
-        user_key = get_key_from_id(index.id_mapping_reverse, cid+1)
-        push!(results, (user_key, cid+1))
-    end
+    results = [pair[2] for pair in dist_id_pairs[1:k]]
     return results
 end
 
 
 function _prune_neighbors(db::SQLite.DB, node_id::String, candidates::Vector{String})
-    maxdegree, indexvecs = getmaxdegreefromdb #TODO:  #TODO: use max degree constant
-    if length(candidates) <= index.maxdegree #TODO: use max degree constant
+
+    if length(candidates) <= DEFAULT_MAX_DEGREE
         return candidates
     end
     
-    node_vec = index.vecs[:, node_id+1] #give it the index vecs for node TODO: 
-    dist_id_pairs = Vector{Tuple{T,Int}}()
-    for cand_id in candidates
-        cand_vec = index.vecs[:, cand_id+1]
+    node_dict = WunDeeDB.get_embeddings(db, [node_id])
+    if !haskey(node_dict, node_id)
+        return candidates
+    end
+        
+    node_vec = node_dict[node_id] 
+    dist_id_pairs = dist_id_pairs = Vector{Tuple{Float64,String}}()
+
+    for cand_id in candidates        
+        cand_dict = WunDeeDB.get_embeddings(db, [cand_id])
+
+        if !haskey(cand_dict, cand_id)
+            continue
+        end
+        
+        cand_vec = cand_dict[cand_id]        
         d = WunDeeDB.compute_distance(cand_vec, node_vec, "euclidean")
         push!(dist_id_pairs, (d, cand_id))
     end
+    
     sort!(dist_id_pairs, by=x->x[1])
-    return [p[2] for p in dist_id_pairs[1:index.maxdegree]]
+    max_return = min(DEFAULT_MAX_DEGREE, length(dist_id_pairs))
+    return [p[2] for p in dist_id_pairs[1:max_return]]
 end
 
 
@@ -180,10 +223,10 @@ function insert!(db::SQLite.DB, node_id::String)
     
     local_vec = node_dict[node_id]
 
-    df = DBInterface.execute(db, "SELECT entrypoint FROM $(WunDeeDB.LM_DISKANN_CONFIG_TABLE_NAME) LIMIT 1") |> DataFrame
+    df = DBInterface.execute(db, "SELECT entrypoint FROM $(LM_DISKANN_CONFIG_TABLE_NAME) LIMIT 1") |> DataFrame
 
     if isempty(df)
-        DBInterface.execute(db, "INSERT OR REPLACE INTO $(WunDeeDB.LM_DISKANN_CONFIG_TABLE_NAME) (entrypoint) VALUES (?)", (node_id,) )
+        DBInterface.execute(db, "INSERT OR REPLACE INTO $(LM_DISKANN_CONFIG_TABLE_NAME) (entrypoint) VALUES (?)", (node_id,) )
         _set_neighbors(db, node_id, String[])
         return
     end
@@ -191,7 +234,7 @@ function insert!(db::SQLite.DB, node_id::String)
     current_entrypoint = df[1, :entrypoint]
 
     if length(current_entrypoint) == 0
-        DBInterface.execute(db, "UPDATE $(WunDeeDB.LM_DISKANN_CONFIG_TABLE_NAME) SET entrypoint = ?", (node_id,))
+        DBInterface.execute(db, "UPDATE $(LM_DISKANN_CONFIG_TABLE_NAME) SET entrypoint = ?", (node_id,))
         _set_neighbors(db, node_id, String[])
         return
     end
@@ -223,13 +266,13 @@ function delete!(db::SQLite.DB, node_id::String)
 
     _set_neighbors(db, node_id, String[])
 
-    df = DBInterface.execute(db, "SELECT entrypoint FROM $(WunDeeDB.LM_DISKANN_CONFIG_TABLE_NAME) LIMIT 1") |> DataFrame
+    df = DBInterface.execute(db, "SELECT entrypoint FROM $(LM_DISKANN_CONFIG_TABLE_NAME) LIMIT 1") |> DataFrame
     
     if !isempty(df)
         current_entrypoint = df[1, :entrypoint]
 
         if current_entrypoint == node_id
-            df2 = DBInterface.execute(db, "SELECT id_text FROM $(WunDeeDB.LM_DISKANN_INDEX_TABLE_NAME) WHERE id_text != ? LIMIT 1", (node_id,)) |> DataFrame
+            df2 = DBInterface.execute(db, "SELECT id_text FROM $(LM_DISKANN_INDEX_TABLE_NAME) WHERE id_text != ? LIMIT 1", (node_id,)) |> DataFrame
             new_entrypoint = isempty(df2) ? "" : df2[1, :id_text]
 
             DBInterface.execute(db, UPDATE_LM_DISKANN_CONFIG_STMT, (new_entrypoint,))
